ddb308a3a3ce8d31799eb46658b1bc0a0eb40bb4
mavis829 <45999053+mavis829@users.noreply.github.com>
2026-09-24 16:50:28 +0800

    Delete 0 JS Basics /0.1 Variables and Operators directory

--- 0 JS Basics /0.1 Variables and Operators/javascript.js
+++
@@ -1,6 +0,0 @@
-// An external script file. Link it from HTML with:
-//   <script src="javascript.js"></script>
-//
-// Use this for the assignment exercises if you prefer a separate file.
-
-console.log("Hello from javascript.js");
