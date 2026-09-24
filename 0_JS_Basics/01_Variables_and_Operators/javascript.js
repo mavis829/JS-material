53e93e2dda40bbb75337c4459eac0365248572b1
mavis829 <45999053+mavis829@users.noreply.github.com>
2026-09-24 16:54:22 +0800

    Add files via upload

---
+++ 0_JS_Basics/01_Variables_and_Operators/javascript.js
@@ -0,0 +1,6 @@
+// An external script file. Link it from HTML with:
+//   <script src="javascript.js"></script>
+//
+// Use this for the assignment exercises if you prefer a separate file.
+
+console.log("Hello from javascript.js");
